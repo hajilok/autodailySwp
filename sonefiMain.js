f2b2cf178b0573ccff2aa8ab0c966bda5312e661
hajilok <120608486+hajilok@users.noreply.github.com>
2024-09-23 01:00:36 +0700

    update sonefi

---
+++ sonefiMain.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+
+import swapMinato from "./src/minato/swap";
+dotenv.config()
+
+let count = 0;
+const maxCount = 3;
+const resetInterval = 24 * 60 * 60 * 1000; // 24 jam dalam milidetik
+
+const main = async (priv) => {
+    if (count < maxCount) {
+        try {
+            const getSwapMinato = await swapMinato(priv)
+            console.log(`Success Daily Swap From Minato https://explorer-testnet.soneium.org/tx/${getSwapMinato.receipt.logs[0].transactionHash}`)
+
+            count++;
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        console.log("Sudah selesai daily nya nunggu reset");
+    }
+    
+
+
+    setTimeout(() => main(priv), 60000); // Panggil lagi setelah 10 menit
+}
+
+// Reset count setiap 24 jam
+setInterval(() => {
+    count = 0;
+    console.log("Daily sudah ready Waktunya bekerja");
+}, resetInterval);
+
+const key = process.env.PRIVATE_KEY
+main(key)
